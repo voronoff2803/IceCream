81079359a6f2a636ac05d7a1ce73796f66a972ac
race <racechao@gmail.com>
2022-08-11 12:15:30 +0800

    Fix duplicates in list checking

--- IceCream/Classes/PendingRelationshipsWorker.swift
+++ IceCream/Classes/PendingRelationshipsWorker.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Soledad on 2021/2/7.
 //
@@ -10,15 +10,14 @@ import RealmSwift
 
 /// PendingRelationshipsWorker is responsible for temporarily storing relationships when objects recovering from CKRecord
 final class PendingRelationshipsWorker<Element: Object> {
-    
     var realm: Realm?
-    
+
     var pendingListElementPrimaryKeyValue: [AnyHashable: (String, Object)] = [:]
-    
+
     func addToPendingList(elementPrimaryKeyValue: AnyHashable, propertyName: String, owner: Object) {
         pendingListElementPrimaryKeyValue[elementPrimaryKeyValue] = (propertyName, owner)
     }
-    
+
     func resolvePendingListElements() {
         guard let realm = realm, pendingListElementPrimaryKeyValue.count > 0 else {
             // Maybe we could add one log here
@@ -28,10 +27,12 @@ final class PendingRelationshipsWorker<Element: Object> {
             for (primaryKeyValue, (propName, owner)) in self.pendingListElementPrimaryKeyValue {
                 guard let list = owner.value(forKey: propName) as? List<Element> else { return }
                 if let existListElementObject = realm.object(ofType: Element.self, forPrimaryKey: primaryKeyValue) {
-                    // Prevent duplicates in list
-                    guard let _ = list.firstIndex(of: existListElementObject) else { continue }
-                    try! realm.write {
-                        list.append(existListElementObject)
+                    if let _ = list.firstIndex(of: existListElementObject) {
+                        // if exist in list, don't append
+                    } else {
+                        try! realm.write {
+                            list.append(existListElementObject)
+                        }
                     }
                     self.pendingListElementPrimaryKeyValue[primaryKeyValue] = nil
                 } else {
@@ -40,5 +41,4 @@ final class PendingRelationshipsWorker<Element: Object> {
             }
         }
     }
-    
 }
